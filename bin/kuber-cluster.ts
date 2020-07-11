55ba9158f7e562d772bcae7434312734f322d66b
Alexey Kurin <a.kurin@neo100.io>
2020-07-11 20:49:01 +0300

    Add bucket export

--- bin/kuber-cluster.ts
+++ bin/kuber-cluster.ts
@@ -1,7 +1,12 @@
 #!/usr/bin/env node
 import 'source-map-support/register';
 import * as cdk from '@aws-cdk/core';
-import { KuberClusterStack } from '../lib/kuber-cluster-stack';
+import {KuberClusterStack} from '../lib/kuber-cluster-stack';
 
 const app = new cdk.App();
-new KuberClusterStack(app, 'KuberClusterStack');
+new KuberClusterStack(app, 'kuber-cluster', {
+    env: {
+        account: "573446657092",
+        region: "us-east-1"
+    }
+});
--- lib/kuber-cluster-stack.ts
+++ lib/kuber-cluster-stack.ts
@@ -1,9 +1,13 @@
 import * as cdk from '@aws-cdk/core';
+import {Bucket} from "@aws-cdk/aws-s3";
+import {CfnOutput} from "@aws-cdk/core";
 
 export class KuberClusterStack extends cdk.Stack {
-  constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
-    super(scope, id, props);
+    constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
+        super(scope, id, props);
 
-    // The code that defines your stack goes here
-  }
+        const kopsBucket = new Bucket(this, 'KuberBucket', {versioned: true});
+
+        new CfnOutput(this, "KopsBucket", {value: kopsBucket.bucketName});
+    }
 }
